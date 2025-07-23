eb26d231a67a12ae11106ed23d6037332386c604
david-tsankov <david.tsankov@gmail.com>
2025-07-23 23:19:46 +0200

    gui

--- linear_operations.py
+++ linear_operations.py
@@ -1,2 +1,5 @@
-"""Add linear operations with vectors, matrices, solving linear equations, eigenvalues, eigenvectors
-Visualize using Manim module and maybe make a GUI thingy."""
+"""
+Add linear operations with vectors, matrices, solving linear equations, eigenvalues, eigenvectors
+Visualize using Manim module
+Add a GUI that can take you to the visualization
+"""

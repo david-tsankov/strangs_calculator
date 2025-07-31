f334027367e6295c1ff5b03bcae940df2e46d032
david-tsankov <david.tsankov@gmail.com>
2025-07-31 17:21:47 +0300

    created package and added some functions, main app.py to execute calculator

--- linear_operations.py
+++
@@ -1,5 +0,0 @@
-"""
-Add linear operations with vectors, matrices, solving linear equations, eigenvalues, eigenvectors
-Visualize using Manim module
-Add a GUI that can take you to the visualization
-"""
---
+++ package/linear_operations.py
@@ -0,0 +1,21 @@
+import numpy as np
+"""
+Add linear operations with vectors, matrices, solving linear equations, eigenvalues, eigenvectors
+Visualize using Manim module
+Add a GUI that can take you to the visualization
+"""
+
+def linear_combination_vectors():
+    pass
+
+def solve_system_of_equations():
+    pass
+
+def matrix_multiplication():
+    pass
+
+def transpose():
+    pass
+
+def get_elimination_matrix():
+    pass

3344bfc1715950eab9465297536211647619ea36
david-tsankov <david.tsankov@gmail.com>
2025-08-04 18:17:31 +0300

    started adding interface for the calculator

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 #File to run app and interface
 
 import package.functions
+import package.linear_operations
+
+print(f"{"Welcome to Strangs Calculator":^100}")
+print(f"{100*"-"}")
+print(f"{"Keep in mind to perform any operation you must first construct a vector/matrix!":^100}")
+print(f"{100*"-"}")
+print(f"{"Choose what operation you would like to perform:":^100}")
+print(f"{"1 - Construct a vector":<25}{"2 - Construct a matrix":<25}{}")
+
 

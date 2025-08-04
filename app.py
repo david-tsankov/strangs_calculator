226edd0dc9ffa8f9def9bf58c53fa41c3b2df6a8
david-tsankov <david.tsankov@gmail.com>
2025-08-04 18:20:33 +0300

    updated interface

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ print(f"{100*"-"}")
 print(f"{"Keep in mind to perform any operation you must first construct a vector/matrix!":^100}")
 print(f"{100*"-"}")
 print(f"{"Choose what operation you would like to perform:":^100}")
-print(f"{"1 - Construct a vector":<25}{"2 - Construct a matrix":<25}{}")
+print(f"{"1 - Construct a vector":<25}{"2 - Construct a matrix":<25}{"3 - Transpose":<25}{"4 - Get lenght":<25}")
+
 
 

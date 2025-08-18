15939b3e5f65620d3a8b5b55ac7c666ab1a25f83
david-tsankov <david.tsankov@gmail.com>
2025-08-18 16:13:05 +0300

    created new class Vector_Manager than inherits from Vector class

--- linear_modules/class_vectors.py
+++ linear_modules/class_vectors.py
@@ -1,68 +1,90 @@
 import numpy as np
 
 class Vector:
-    vectors={
-
-    }
-    vector_objects={
-
-    }
-
-    def __init__(self,vector, name):
-        self.vector = vector
+    def __init__(self, components, name):
+        self.components = components
         self.name = name
 
     def vector_construct(self):
-        self.vector=np.array(self.vector)
-        Vector.vectors[self.name]=self.vector.tolist()
-        Vector.vector_objects[self.name]=self
-
-    def vectors_list():
-        for name, vector in Vector.vectors.items():
-            print(f"{name}")
-            for component in vector:
-                print(f" {component}")
+        self.components=np.array(self.components)
 
     def __str__(self):
-        return f"{self.vector}"
+        return f"{self.name} =\n{self.components}"
     
     def get_lenght(self):
-        norm=np.linalg.norm(self.vector)
+        norm=np.linalg.norm(self.components)
         return norm
     
     def transpose(self):
-        vector_transpose=np.transpose(self.vector)
+        vector_transpose=np.transpose(self.components)
         return vector_transpose
     
     def scalar_multiplication(self, scalar: float=1):
-        new_vector=np.dot(self.vector, scalar)
+        new_vector=np.dot(self.components, scalar)
         return new_vector
     
     def linear_combination(self,other: "Vector",scalar_self: float=1,scalar_other:float=1):
-        linear_comb=np.add(self.vector*scalar_self,other.vector*scalar_other)
+        linear_comb=np.add(self.components*scalar_self,other.components*scalar_other)
         return linear_comb
     
     def normalize(self):
-        normalized_vector=self.vector/np.linalg.norm(self.vector)
+        normalized_vector=self.components/np.linalg.norm(self.components)
         return normalized_vector
     
+    # def dot_product(self,other: "Vector",self_scalar:float=1,other_scalar:float=1):
+    #     """Input must be two vectors of the same shape, as function takes care of transposition and shape alignment."""
+    #     self.components=np.transpose(self.components)
+    #     dot_product=np.dot(self.components*self_scalar,other.components*other_scalar)
+    #     return dot_product
+    
+    # def cross_product(self,self.name other: "Vector"):
+    #     """The function takes only vertical vectors with 3 components (3D), """
+    #     if self.components.shape[0]==3 and other.components.shape[0]==3:
+    #         self.components=np.transpose(self.components)
+    #         other.components=np.transpose(other.components)
+    #         cross_product=np.cross(self.components,other.components)
+    #         return np.transpose(cross_product)
+    #     else: 
+    #         return "Either vectors aren't vertical, or vectors aren't 3D, if you wish to use 2D vectors, append a [0] as a third component on each vector!"
+        
+class Vector_Manager(Vector):
+    vector_objects={
+
+    }
+    def __init__(self, components, name):
+        super().__init__(components, name)
+
+    def object_saver(self):
+        Vector_Manager.vector_objects[self.name]=self
+
     def dot_product(self,other: "Vector",self_scalar:float=1,other_scalar:float=1):
         """Input must be two vectors of the same shape, as function takes care of transposition and shape alignment."""
-        self.vector=np.transpose(self.vector)
-        dot_product=np.dot(self.vector*self_scalar,other.vector*other_scalar)
+        self.components=np.transpose(self.components)
+        dot_product=np.dot(self.components*self_scalar,other.components*other_scalar)
         return dot_product
     
     def cross_product(self, other: "Vector"):
         """The function takes only vertical vectors with 3 components (3D), """
-        if self.vector.shape[0]==3 and other.vector.shape[0]==3:
-            self.vector=np.transpose(self.vector)
-            other.vector=np.transpose(other.vector)
-            cross_product=np.cross(self.vector,other.vector)
+        if self.components.shape[0]==3 and other.components.shape[0]==3:
+            self.components=np.transpose(self.components)
+            other.components=np.transpose(other.components)
+            cross_product=np.cross(self.components,other.components)
             return np.transpose(cross_product)
         else: 
             return "Either vectors aren't vertical, or vectors aren't 3D, if you wish to use 2D vectors, append a [0] as a third component on each vector!"
+    
+    def linear_combination(self,other: "Vector",scalar_self: float=1,scalar_other:float=1):
+        linear_comb=np.add(self.components*scalar_self,other.components*scalar_other)
+        return linear_comb
+    
+    def list_vectors():
+        for name, object in Vector_Manager.vector_objects.items():
+            print(f"{name} =\n{print(object)}")
+        return
         
         
+        
+
         
     
 if __name__=="__main__":
@@ -70,5 +92,15 @@ if __name__=="__main__":
     x.vector_construct()
     B=Vector([[2],[-1],[5]],"B")
     B.vector_construct()
-    Vector.vectors_list()
-    print(Vector.get_lenght(x))
+    # print(x)
+    # print(B)
+
+    x=Vector_Manager([[1],[0],[0]],"x")
+    Vector_Manager.vector_construct(x)
+    Vector_Manager.object_saver(x)
+    B=Vector_Manager([[2],[-1],[5]],"B")
+    Vector_Manager.vector_construct(B)
+    Vector_Manager.object_saver(B)
+    print(Vector_Manager.vector_objects)
+    print(Vector_Manager.list_vectors())
+    
